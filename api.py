95213a9e06b272919e3b116127009bc3f4936c69
dad2jrn <dad2jrn@gmail.com>
2023-08-19 19:19:43 -0400

    initial create & structure

---
+++ api.py
@@ -0,0 +1,30 @@
+from typing import Optional, Any
+import json
+import requests
+
+class WeatherApp:
+
+    def __init__(self):
+        pass
+
+    def read_api_key(self) -> Optional[str]:
+        """Reads the API key from the provided file.
+
+        Returns:
+        - The API key as a string if successful, or None otherwise.
+        """
+        with open(self.api_key_file, 'r') as f:
+            return f.read().strip()
+
+    def get_ip_location(self) -> Optional[str]:
+        pass
+
+    def get_weather_data(self, query: str) -> Optional[Any]:
+        pass
+
+    def run(self):
+        pass
+
+if __name__ == "__main__":
+    app = WeatherApp()
+    app.run()
